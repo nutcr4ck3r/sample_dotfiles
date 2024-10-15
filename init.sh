3dfbec77520ada856d88b29e3b8f6a8ee297ab86
nutcr4ck3r <102888519+nutcr4ck3r@users.noreply.github.com>
2024-10-15 22:32:46 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,22 +8,13 @@ echo "[i] Install basic tools."
 sudo apt install -y \
   xclip source-highlight docker.io docker-compose \
   gawk tmux unar ripgrep bat wget curl git unzip ranger \
-  python3-pip python3-venv
+  python3-pip python3-venv fzf
 
 # Install glow to enable syntax highlighting on markdown files
 wget https://github.com/charmbracelet/glow/releases/download/v1.5.1/glow_1.5.1_amd64.deb \
   -O ~/Downloads/glow.deb
 sudo dpkg -i ~/Downloads/glow.deb
 rm ~/Downloads/glow.deb
-
-# Install peco to search command histories
-wget https://github.com/peco/peco/releases/download/v0.5.11/peco_linux_amd64.tar.gz \
-  -O ~/Downloads/peco.tar.gz
-tar -zxvf ~/Downloads/peco.tar.gz -C ~/Downloads/
-mkdir -p ~/.local/bin
-move ~/Downloads/peco_linux_amd64/peco ~/.local/bin/
-rm ~/Downloads/peco.tar.gz
-rm -r ~/Downloads/peco_linux_amd64
 
 # Download & install HackGen Console NF
 wget https://github.com/yuru7/HackGen/releases/download/v2.9.0/HackGen_NF_v2.9.0.zip \
@@ -33,6 +24,10 @@ mkdir -p ~/.fonts
 mv ~/Downloads/HackGen_NF_v2.9.0/Hack* ~/.fonts/
 rm ~/Downloads/Hack.zip
 rm -r ~/Downloads/HackGen_NF_v2.9.0
+
+# vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Localization (Japanese font, Timezone)
 sudo apt install -y fonts-ipafont fcitx-mozc
