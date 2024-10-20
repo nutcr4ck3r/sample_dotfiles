76728828128736494494b3b06221692c035143ce
nutcr4ck3r <102888519+nutcr4ck3r@users.noreply.github.com>
2024-10-20 17:52:50 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,17 +8,18 @@ echo "[i] Install basic tools."
 sudo apt install -y \
   xclip source-highlight docker.io docker-compose \
   gawk tmux unar ripgrep bat wget curl git unzip ranger \
-  python3-pip python3-venv fzf bat
+  python3-pip python3-venv fzf vim-gtk3
 
-# Create .local/bin directory & batcat link for fzf/rg
+# Create link for batcat
 mkdir -p ~/.local/bin
 ln -s /usr/bin/batcat ~/.local/bin/bat
 
-# Install glow to enable syntax highlighting on markdown files
-wget https://github.com/charmbracelet/glow/releases/download/v1.5.1/glow_1.5.1_amd64.deb \
-  -O ~/Downloads/glow.deb
-sudo dpkg -i ~/Downloads/glow.deb
-rm ~/Downloads/glow.deb
+# vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# pyenv
+curl https://pyenv.run | bash
 
 # Download & install HackGen Console NF
 wget https://github.com/yuru7/HackGen/releases/download/v2.9.0/HackGen_NF_v2.9.0.zip \
@@ -28,13 +29,6 @@ mkdir -p ~/.fonts
 mv ~/Downloads/HackGen_NF_v2.9.0/Hack* ~/.fonts/
 rm ~/Downloads/Hack.zip
 rm -r ~/Downloads/HackGen_NF_v2.9.0
-
-# vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# pyenv
-curl https://pyenv.run | bash
 
 # Localization (Japanese font, Timezone)
 sudo apt install -y fonts-ipafont fcitx-mozc
@@ -52,8 +46,20 @@ echo "[i] Change login shell to zsh."
 echo "[i] Enter your login user name."
 read -r username
 sudo chsh $username -s /usr/bin/zsh
-# git clone --depth 1 -- https://github.com/marlonrichert/zsh-snap.git
-# source zsh-snap/install.zsh
+
+
+#########
+## git ##
+################################################################################
+echo ""
+echo "[i] Set git info."
+echo "[i] Enter git email address."
+read -r mail
+git config --global user.email $mail
+echo "[i] Enter git username."
+read -r name
+git config --global user.email $name
+git config --global core.quotepath false
 
 
 ##########
